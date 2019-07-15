2433d8f9f2dc78f9132d666732ffb4b587f85525
David Abiola <daveabiola@gmail.com>
2019-07-15 19:05:15 +0100

    chore(backend-dependencies): setting up backend dependencies
    - configure CI
    - add CI and code coverage badges
    - setup documentation route
    - setup server
    
    [Finishes #167279428]

---
+++ backend/src/config/app.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import appRoot from 'app-root-path';
+
+dotenv.config();
+// export app configuration object module
+const config = {
+  APP_NAME: process.env.APP_NAME,
+  ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT,
+  HOST: process.env.HOST,
+  LOG_LEVEL: process.env.LOG_LEVEL,
+  APP_ROOT: appRoot,
+  JWT_KEY: process.env.JWT_KEY,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  DB_PORT: process.env.DB_PORT,
+  DATABASE_URL: process.env.DATABASE_URL,
+  TEST_DB_URL: process.env.TEST_DB_URL,
+};
+
+export default config;
